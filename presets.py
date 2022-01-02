04b0a0c1e7343102e2f296a92a8bf0eb3c7264b4
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-01-02 11:20:23 +0530

    Add files via upload

--- presets.py
+++ presets.py
@@ -14,50 +14,50 @@ class Presets(object):
 <b>/subs</b><code> - to count subscribers</code>
 <b>/send</b><code>  - to broadcast (as reply)</code>
 
-<b>Join:</b><a href='https://t.me/RMProjects'><b> Support</b></a> | Repo\
-<a href='https://github.com/m4mallu/inline-tube-mate'><b> LINK</b></a>   
+<b>Join:</b><a href='https://t.me/DK_BOTZ'><b> Support Group</b></a> | Channel\
+<a href='https://t.me/DKBOTZ'><b> DKBOTZ</b></a>   
     """
 
     OPTIONS_TXT = "<code>🔰 Select the option 🔰</code>"
-    WELCOME_MSG = "<code>Hello...</code><b>{}</b> 👋\n<code>I'm a YouTube downloader bot with lot's of features. " \
-                  "Search inline for videos and click to proceed for download.</code>"
+    WELCOME_MSG = "<code>Hello...</code><b>{}</b> 👋\n<code>I'm a YouTube Downloader Bot With Lot's Of Features. " \
+                  "Search inline For Videos And Click To Proceed For Download.\n For More Help Send /help</code>"
     RESULTS_TXT = "👀 Results:"
     NO_RESULTS = "❌ No Results"
     DESCRIPTION = "Duration: {} || {}"
     NOT_AUTH_TXT = "<b>Error : </b>\n\n<code>You are not Authorized to use this bot.</code>"
-    DEFAULT_TITLE = "Inline Tube Mate Repository"
+    DEFAULT_TITLE = "YOUTUBE DOWNLOADER BOT"
     DEFAULT_THUMB_URL = "https://image.flaticon.com/icons/png/512/25/25231.png"
-    DEFAULT_LINK = "https://github.com/m4mallu/inline-tube-mate"
-    DEFAULT_DESCRIPTION = "Link: M4mallu | GitHub"
+    DEFAULT_LINK = "https://youtube.com/"
+    DEFAULT_DESCRIPTION = "Link: @DKBOTZ | TELEGRAM"
     DEV_TITLE = "Developer Information"
     DEV_THUMB_URL = "https://freepikpsd.com/media/2019/10/software-developer-icon-png-2-Transparent-Images.png"
-    DEV_LINK = "https://t.me/space4renjith"
-    DEV_DESCRIPTION = "Name: Renjith Mangal | Telegram"
-    SHARE_BUTTON_TEXT = "Hi.. 👋\nCheckout : @{username}\nFor search and download TouTube Videos"
+    DEV_LINK = "https://t.me/DKBOTZHELP"
+    DEV_DESCRIPTION = "Name: Anonymous | Telegram"
+    SHARE_BUTTON_TEXT = "Hi  👋\nI FOUND MOST POWERFUL YOUTUBE DOWNLOADER BOT | LINK :-  @{username}\nFor search and download YouTube Videos"
     SAVED_THUMB = "<b>✅ Thumbnail Saved Successfully</b>\n<code>This file will be used in next YouTube " \
                   "downloads until you clear it !</code> "
     WAIT_MESSAGE = "<code>Processing...</code>"
-    THUMB_CAPTION = "<code>This image is your current thumbnail, Tap </code><b> DEL THUMB </b><code> if you wish to " \
-                    "clear it !</code> "
+    THUMB_CAPTION = "<code>This image is Your Current Thumbnail, Tap </code><b> DEL THUMB </b><code> if You Wish To " \
+                    "Clear it !</code> "
     NO_THUMB = "❌ Can't find any thumbnail in your local, Please upload an image to set it.."
     DEL_THUMB_CNF = "Thumbnail Cleared Successfully ✅"
-    LINK_ERROR = "<b>Error : </b>\n\n<code>Some error occurred while the process !\n Plz try again later..</code>"
+    LINK_ERROR = "<b>Error : </b>\n\n<code>Some Error Occurred While The Process !\n Please Try Again Later..</code>"
     NO_VOID_FORMAT_FOUND = "<code>{}</code>"
-    FINISHED_PROGRESS_STR = "◼️"
-    UN_FINISHED_PROGRESS_STR = "◻️"
-    SOURCE_URL = "https://github.com/m4mallu/inline-tube-mate"
-    SUPPORT_URL = "https://t.me/RMProjects"
+    FINISHED_PROGRESS_STR = "🍊"
+    UN_FINISHED_PROGRESS_STR = "🍋"
+    SOURCE_URL = "https://t.me/DK_BOTZ"
+    SUPPORT_URL = "https://t.me/DK_BOTZ"
     CHECKING_LINK = "⏳ <code>Analyzing your link...</code>"
     DOWNLOAD_START = "⬇️ <code>Download Initiated...</code>"
     UPLOAD_START = "⬆️ <code>Upload Initiated...</code>"
-    NOT_DOWNLOADABLE = "<b>This URL is not downloadable !</b> 🙄"
+    NOT_DOWNLOADABLE = "<b>This URL Is Not Downloadable !</b> 🙄"
     CANCEL_PROCESS = "<b>Process Cancelled Successfully</b>  ✅"
     SEND_TEXT = "<b>Processing...</b>\n<i>This message will automatically disappear when the broadcasting is " \
                 "finished</i> "
     REPLY_ERROR = "<i>Use this command as a replay to any telegram message with out any spaces.</i>"
     USERS_LIST = "<b>Total: {}</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
-    WAIT_MSG = "<b>Processing...</b>\n<i>This will take some time...</i>"
-    PROMPT_THUMB = "<b>Do you want to set this image as a thumbnail?</b>"
+    WAIT_MSG = "<b>Processing...</b>\n<i>This Will Take Some Time...</i>"
+    PROMPT_THUMB = "<b>Do You Want To Set This image As A Thumbnail?</b>"
     FORMAT_SELECTION = """
 <code>Title - </code><b>{}</b>
 
@@ -67,19 +67,19 @@ class Presets(object):
 
 👇<code>Select the required format</code>👇
     """
-    NOT_SUB_TXT = "<b>Oopz </b> 😯\n\n<code>In order to use this feature, You need to join my channel.</code>"
+    NOT_SUB_TXT = "<b>Hi  👋</b> **Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!</code>"
     BOT_NOT_PRESENT = "<b>Error : </b>\n\n<code>Bot need to be an admin to the force subscribe chat !</code>"
-    NO_INVITE_METHOD = "<b>Error : </b>\n\n<code>Chat invite method not found ! Try to generate an invite " \
-                       "link in your force subscribe chat.</code>"
+    NO_INVITE_METHOD = "<b>Error : </b>\n\n<code>Chat invite Method Not Found ! Try to generate an invite " \
+                       "JOIN :- @DKBOTZ AND SEND /start</code>"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | newfilename | username | password"""
     CUSTOM_CAPTION_UL_FILE = "\xad \xad\n<b>{}</b>\n\n<b>DL Credits: </b><b><a " \
                              "href='https://github.com/m4mallu'> M4Mallu</a></b> "
-    RCHD_TG_API_LIMIT = "Detected File Size: {}\n\nSorry. But, I cannot upload files " \
-                        "greater than 1.95GB due to Telegram API limitations."
-    AD_STRING_TO_REPLACE = "please report this issue on https://yt-dl.org/bug . Make sure you are using the " \
+    RCHD_TG_API_LIMIT = "Detected File Size: {}\n\nSorry. But, I Cannot Upload Files " \
+                        "Beacause File Greater Than 1.95GB Due To Telegram API Limitations."
+    AD_STRING_TO_REPLACE = "Please Report this issue on https://yt-dl.org/bug . Make sure you are using the " \
                            "latest version; see  https://yt-dl.org/update  on how to update. Be sure to call " \
                            "youtube-dl with the --verbose flag and include its complete output."
     INITIAL_MEDIA = "https://telegra.ph/file/c3f88ba6883554654fb4e.png"
     ERROR_MEDIA = "https://telegra.ph/file/27fd810c591c884dba11d.jpg"
-    SETTINGS = "<code>Need an update in settings?</code>"
+    SETTINGS = "<code>Need An Update in Settings?</code>"
